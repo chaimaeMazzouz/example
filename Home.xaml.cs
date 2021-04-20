022ef47412d8564d1682f623cc49c47ae15c05ef
chaimaeMazzouz <mazzouz.chaimae.dev@gmail.com>
2021-04-20 22:20:59 +0000

    add Login, Home, Permis_Recherche interface graphique

--- Home.xaml.cs
+++ Home.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace GestionMines
 {
     /// <summary>
-    /// Logique d'interaction pour MainWindow.xaml
+    /// Logique d'interaction pour Home.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Home : Window
     {
-        public MainWindow()
+        public Home()
         {
             InitializeComponent();
         }
---
+++ Permis_Recherche.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GestionMines
+{
+    /// <summary>
+    /// Logique d'interaction pour Permis_Recherche.xaml
+    /// </summary>
+    public partial class Permis_Recherche : Window
+    {
+        public Permis_Recherche()
+        {
+            InitializeComponent();
+        }
+    }
+}
